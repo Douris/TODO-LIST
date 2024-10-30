8ef0ff309b26bb7b82e3911758c939b514590a03
Oury Balde <ouryboulloye@gmail.com>
2024-10-30 09:25:21 +0100

    final project

---
+++ app.js
@@ -0,0 +1,51 @@
+let taskList = document.getElementById('taskList');
+
+function addTask(){
+    let taskInput = document.getElementById('taskInput');
+    let taskText = taskInput.value;
+    let li = document.createElement('li');
+    let editButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
+
+    if(taskText === ""){
+        return;
+    }
+
+    li.innerHTML = taskText;
+
+    editButton.innerHTML = '<ion-icon name="pencil-outline" class="modify"></ion-icon>' ;  
+   
+    editButton.onclick = function(){
+        editTask(li);
+    } 
+
+    deleteButton.innerHTML = '<ion-icon name="trash-outline" class="delete"></ion-icon>';
+     
+    deleteButton.onclick = function(){
+        deleteTask(li);
+    }
+
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+
+    taskList.appendChild(li);
+
+    taskInput.value ="";
+}
+
+function editTask(task){
+    let taskTextElement = task.firstChild;
+    let taskText = taskTextElement.textContent;
+
+    let newTaskText = prompt('Modifier la tâche:', taskText);
+
+    if(newTaskText === null || newTaskText === ""){
+        return;
+    } 
+
+    taskTextElement.textContent = newTaskText;
+}
+
+function deleteTask(task){
+    taskList.removeChild(task);
+}
